fbe2b416f3d8e20738933955baf56b46016e067e
Leonard Atorough <leotorough@gmail.com>
2026-09-11 18:34:52 +0100

    fmt fixes

--- cmd/game/systems/movement.go
+++ cmd/game/systems/movement.go
@@ -15,25 +15,25 @@ func (s *MovementSystem) Init(world *ecs.World) error {
 }
 
 func (s *MovementSystem) Update(world *ecs.World, delta float64) error {
-    for entry := range world.NewQuery().
-        WithRequiredComponents(components.Transform{}, gamecomponents.Velocity{}).
-        Execute() {
+	for entry := range world.NewQuery().
+		WithRequiredComponents(components.Transform{}, gamecomponents.Velocity{}).
+		Execute() {
 
-        id := entry.EntityID
-        vel, _ := entry.Get[gamecomponents.Velocity]()
-        transform, _ := entry.Get[components.Transform]()
+		id := entry.EntityID
+		vel, _ := entry.Get[gamecomponents.Velocity]()
+		transform, _ := entry.Get[components.Transform]()
 
-        // Apply velocity
-        transform.Position.X += vel.Linear.X * delta
-        transform.Position.Y += vel.Linear.Y * delta
+		// Apply velocity
+		transform.Position.X += vel.Linear.X * delta
+		transform.Position.Y += vel.Linear.Y * delta
 
-        // DELETE: the clamping block (lines 31-42)
+		// DELETE: the clamping block (lines 31-42)
 
-        if err := world.SetComponent(id, transform); err != nil {
-            return err
-        }
-    }
-    return nil
+		if err := world.SetComponent(id, transform); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (s *MovementSystem) Shutdown(world *ecs.World) error {
